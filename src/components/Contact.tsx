ae455830b4f3d6eade7dc205d3def9249bfaafbc
Akshay Vaghasiya <akshayvaghasiya84@gmail.com>
2026-03-09 17:51:37 +0000

    Init Commit

---
+++ src/components/Contact.tsx
@@ -0,0 +1,69 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:info@akshayvaghasiya.com" data-cursor="disable">
+                info@akshayvaghasiya.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>MSc in Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/axayvaghasiya"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/akshayvaghasiya"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              LinkedIn <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/akshay_codes"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/akshayvaghasiya.codes"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Akshay Vaghasiya</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026 Akshay Vaghasiya. All rights reserved.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
